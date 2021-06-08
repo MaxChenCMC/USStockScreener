f39341929e06345782f9783234d05f6cdffc11f5
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2021-06-08 16:59:04 +0800

    Update page0.py

--- page0.py
+++ page0.py
@@ -3,14 +3,16 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import mplfinance as mpf
-import requests, lxml # lxml在本地端不用，但佈屬streamlit雲端就要
+import requests, time, lxml # lxml在本地端不用，但佈屬streamlit雲端就要
 import matplotlib.pyplot as plt
+from io import StringIO
+from bs4 import BeautifulSoup
 
 # Emoji按"Win"鍵加";" 亦可指定png檔／layout可centered
 st.set_page_config(page_icon = '💯', page_title = 'Max選股', layout = 'wide', initial_sidebar_state = 'auto')
 st.markdown("""<style>.main{background-color:#EFE3D8}</style>""", unsafe_allow_html = True)
 
-choice = st.sidebar.selectbox('Menu', options = ['Home', 'US Stock', 'TW Stock', 'How To Learn'], index = 0)
+choice = st.sidebar.selectbox('Menu', options = ['Home', 'US Stock', 'TW Stock', 'Institutional Buyer', 'How To Learn'], index = 0)
 st.sidebar.write('右上角 ≡ ☛ settings ☛ Theme 選 Light 體驗較佳')
 
 if choice == 'Home':
@@ -25,6 +27,10 @@ elif choice == 'TW Stock':
     import page3
     page3.active()
 
-elif choice == 'How To Learn':
+elif choice == 'Institutional Buyer':
     import page4
     page4.active()
+
+elif choice == 'How To Learn':
+    import page5
+    page5.active()
