e11361b3c2fe89fb51749a6d15c583d018249d92
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2021-09-12 23:19:33 +0800

    Add files via upload

---
+++ us_stock.py
@@ -0,0 +1,251 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import mplfinance as mpf
+import matplotlib.pyplot as plt
+import requests, lxml
+
+st.set_option("deprecation.showPyplotGlobalUse", False)  # pyplot那邊很囉嗦
+
+
+def kbar_plot(ticker, start=None, end=None, period=None):
+    df = yf.Ticker(ticker).history(start=start, end=end, period=period).iloc[:, :5]
+    df.columns = df.columns.str.lower()
+    clr = mpf.make_marketcolors(up="g", down="r", edge="k", wick="k")
+    sty = mpf.make_mpf_style(base_mpf_style="default", marketcolors=clr)
+    kwargs = dict(type="candle", volume=True, style=sty)
+    plt.figure(figsize=(15, 5))
+    return mpf.plot(df, **kwargs, title=ticker)
+
+
+def rebase(sids, start=None, end=None):
+    df = pd.DataFrame(
+        {i: yf.Ticker(i).history(start=start, end=end)["Close"] for i in sids}
+    )
+    return ((1 + df.pct_change()).cumprod() - 1).fillna(0.0)
+
+
+def watchlist_us():
+    web = requests.get(
+        "https://www.slickcharts.com/sp500",
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"
+        },
+    ).text
+    sid1 = pd.read_html(web)[0]["Symbol"].apply(lambda x: x.replace(".", "-")).to_list()
+    ark = pd.read_html("https://cathiesark.com/ark-funds-combined/complete-holdings")[
+        0
+    ]["Ticker"][1:31].to_list()
+    my_watchlist = [
+        "AMEH",
+        "APLS",
+        "APPS",
+        "BILI",
+        "CDLX",
+        "DXCM",
+        "ENPH",
+        "LEN",
+        "MRNA",
+        "NET",
+        "NIO",
+        "NVTA",
+        "NXST",
+        "PENN",
+        "PLUG",
+        "QDEL",
+        "SHOP",
+        "SNAP",
+        "THO",
+        "TSLA",
+        "ZM",
+        "ZS",
+        # mkt cap < 5B
+        "AVID",
+        "BGFV",
+        "ATTO",
+        "ASPN",
+        "CLNE",
+        "DAC",
+        "ESTE",
+        "FLGT",
+        "GRWG",
+        "HIMX",
+        "MARA",
+        "NTP",
+        "SBOW",
+        "SPWR",
+        "SCR",
+        "SLCA",
+        "TCS",
+        "TGI",
+        "TIGR",
+    ]
+
+    watchlist_us = list(set(sid1 + ark + my_watchlist))
+    return watchlist_us
+
+
+def active():
+    st.image("./img/cover_us.jpg", use_column_width=True)
+    st.markdown(
+        f"<h1 style= 'text-align: center;'> Max US stocks </h1>", unsafe_allow_html=True
+    )
+
+    st.markdown(
+        """
+    # 動能選股
+    #### 選股範圍
+    [ARK方舟基金前30大持股](https://cathiesark.com/ark-funds-combined/complete-holdings)
+    加 [S&P500成份股](https://www.slickcharts.com/sp500)
+    ####
+    """
+    )
+    criteria = st.multiselect(
+        "最多4個", ["即將創近月新高", "長紅K棒", "爆量", "最近剛黃金交叉"], default=["即將創近月新高", "長紅K棒", "爆量"]
+    )
+    run = st.button("開始選股(需要8~10分鐘)")
+    if run:
+        list_to_trade = []
+        for i in watchlist_us():
+            df = yf.Ticker(i).history(period="2mo").iloc[:, :5]
+            cond1 = (df["Close"] * 1.03 > df["High"].rolling(22).max())[-1]
+            cond2 = (
+                (df["Close"] - df["Open"])
+                > abs(df["Open"] - df["Close"]).rolling(10).mean() * 2.5
+            )[-1]
+            cond3 = (df["Volume"] > df["Volume"].rolling(5).mean() * 1.3)[-1]
+            duo_ma = df["Close"].rolling(5).mean() >= df["Close"].rolling(10).mean()
+            cond4 = (
+                (duo_ma == True) & ((duo_ma != duo_ma.shift()).rolling(5).sum() == 1)
+            )[-1]
+            criteria_ = []
+            if "即將創近月新高" in criteria:
+                criteria_.append(cond1)
+            if "長紅K棒" in criteria:
+                criteria_.append(cond2)
+            if "爆量" in criteria:
+                criteria_.append(cond3)
+            if "最近剛黃金交叉" in criteria:
+                criteria_.append(cond4)
+            if sum(criteria_) == len(criteria):
+                list_to_trade.append(i)
+        if list_to_trade != []:
+            st.text_area(label="選股結果請先複製下來，不然網頁更新時會被洗掉", value=list_to_trade)
+            for i in list_to_trade:
+                kbar_plot(i, period="3mo")
+                st.pyplot()
+        else:
+            st.warning("條件別設太嚴不然選不出東西")
+
+    st.markdown(
+        "------------------------------------------------------------------------------------"
+    )
+    st.header("我有自己想看的")
+    req1, req2, req3 = st.beta_columns(3)
+    start = req1.date_input("Start", value=pd.to_datetime("2021-04-01"), key="1")
+    end = req2.date_input("End", key="2")
+    ticker = req3.text_input("股號", value="GME")
+    run = st.button("OK")
+    if run:
+        kbar_plot(ticker, start=start, end=end)
+        st.pyplot()
+
+    st.markdown(
+        "------------------------------------------------------------------------------------"
+    )
+    st.header("同期強弱勢比較")
+    col1, col2 = st.beta_columns(2)
+    start = col1.date_input("Start", value=pd.to_datetime("2021-01-01"), key="3")
+    end = col2.date_input("End", key="4")
+    sids = st.radio("", ("國際指數", "SPDR 11 SECTORS", "ARKK Top15"))
+    if sids == "國際指數":
+        df = rebase(
+            ["^DJI", "^IXIC", "^GSPC", "^SOX", "^GDAXI", "EWT", "^TWII", "^TWOII"],
+            start=start,
+            end=end,
+        )
+        df.columns = [
+            "DowJones",
+            "Nasdaq",
+            "S&P 500",
+            "PHLX Semiconductor",
+            "DAX Germany",
+            "MSCI Taiwan",
+            "TSE",
+            "OTC",
+        ]
+    elif sids == "SPDR 11 SECTORS":
+        df = rebase(
+            [
+                "XLB",
+                "XLC",
+                "XLE",
+                "XLF",
+                "XLI",
+                "XLK",
+                "XLP",
+                "XLRE",
+                "XLU",
+                "XLV",
+                "XLY",
+            ],
+            start=start,
+            end=end,
+        )
+        df.columns = [
+            "Materials",
+            "Communication Services",
+            "Energy",
+            "Financials",
+            "Industrials",
+            "Technology",
+            "Consumer Staples",
+            "Real Estate",
+            "Utilities",
+            "Health Care",
+            "Consumer Discretionary",
+        ]
+    elif sids == "ARKK Top15":
+        ark = pd.read_html(
+            "https://cathiesark.com/ark-funds-combined/complete-holdings"
+        )[0]["Ticker"][1:16].to_list()
+        df = rebase(ark, start=start, end=end)
+    df.sort_values(by=df.index[-1], ascending=False, axis=1).plot(figsize=(15, 12))
+    plt.legend(loc=2)
+    st.pyplot()  # st.line_chart() 自動生成的legend是照字母排序
+
+    st.markdown(
+        "------------------------------------------------------------------------------------"
+    )
+    st.header("前16大權值股，挑幾檔順眼的來比一比")
+    col1, col2 = st.beta_columns(2)
+    start = col1.date_input("Start", value=pd.to_datetime("2021-01-01"), key="5")
+    end = col2.date_input("End", key="6")
+    blue_chips = [
+        "AAPL",
+        "MSFT",
+        "AMZN",
+        "GOOG",
+        "FB",
+        "TSLA",
+        "TSM",
+        "BABA",
+        "V",
+        "JPM",
+        "JNJ",
+        "WMT",
+        "MA",
+        "NVDA",
+        "UNH",
+        "BA",
+    ]
+    weighted = st.multiselect("", blue_chips, default=["GOOG", "FB", "AAPL", "TSLA"])
+    show = st.button("先挑這些", key="7")
+    if show:
+        df = rebase(weighted, start=start, end=end)
+        df.sort_values(by=df.index[-1], ascending=False, axis=1).plot(figsize=(15, 10))
+        plt.legend(loc=0)
+        st.pyplot()
+
+
+active()
